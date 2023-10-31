7b8107f9d39eab8b73aa5c544d395a87ba6a2c00
Lars Bahner <lars.bahner@gmail.com>
2023-10-31 04:58:50 +0100

    Make relayService mandatory

--- config.go
+++ config.go
@@ -9,29 +9,27 @@ import (
 )
 
 const (
-	defaultLowWaterMark       int           = 100
-	defaultHighWaterMark      int           = 10000
-	defaultLogLevel           string        = "info"
-	defaultConnMgrGrace       time.Duration = time.Minute * 1
-	defaultListenPort         string        = "4001" // 0 = random
-	defaultHttpAddr           string        = "0.0.0.0"
-	defaultHttpPort           string        = "4000"
-	defaultRendezvous         string        = "/ma/0.0.1"
-	defaultDiscoverySleep     time.Duration = time.Second * 10
-	defaultEnableRelayService bool          = false
+	defaultLowWaterMark        int           = 100
+	defaultHighWaterMark       int           = 1000
+	defaultLogLevel            string        = "info"
+	defaultConnMgrGrace        time.Duration = time.Minute * 1
+	defaultListenPort          string        = "4001" // 0 = random
+	defaultHttpAddr            string        = "0.0.0.0"
+	defaultHttpPort            string        = "4000"
+	defaultRendezvous          string        = "/ma/0.0.1"
+	defaultDiscoverySleep      time.Duration = time.Second * 10
 )
 
 var (
-	discoverySleep     time.Duration = env.GetDuration("GO_DHT_SERVER_DISCOVERY_SLEEP", defaultDiscoverySleep)
-	httpAddr           string        = env.Get("GO_DHT_SERVER_HTTP_ADDR", defaultHttpAddr)
-	httpPort           string        = env.Get("GO_DHT_SERVER_HTTP_PORT", defaultHttpPort)
-	listenPort         string        = env.Get("GO_DHT_SERVER_LISTEN_PORT", defaultListenPort)
-	logLevel           string        = env.Get("GO_DHT_SERVER_LOG_LEVEL", defaultLogLevel)
-	lowWaterMark       int           = env.GetInt("GO_DHT_SERVER_LOW_WATER_MARK", defaultLowWaterMark)
-	highWaterMark      int           = env.GetInt("GO_DHT_SERVER_HIGH_WATER_MARK", defaultHighWaterMark)
-	connmgrGracePeriod time.Duration = env.GetDuration("GO_DHT_SERVER_CONN_MGR_GRACE_PERIOD", defaultConnMgrGrace)
-	rendezvous         string        = env.Get("GO_DHT_SERVER_RENDEZVOUS", "")
-	enableRelayService bool          = env.GetBool("GO_DHT_SERVER_ENABLE_RELAY", defaultEnableRelayService)
+	discoverySleep      time.Duration = env.GetDuration("GO_MA_RELAY_DISCOVERY_SLEEP", defaultDiscoverySleep)
+	httpAddr            string        = env.Get("GO_MA_RELAY_HTTP_ADDR", defaultHttpAddr)
+	httpPort            string        = env.Get("GO_MA_RELAY_HTTP_PORT", defaultHttpPort)
+	listenPort          string        = env.Get("GO_MA_RELAY_LISTEN_PORT", defaultListenPort)
+	logLevel            string        = env.Get("GO_MA_RELAY_LOG_LEVEL", defaultLogLevel)
+	lowWaterMark        int           = env.GetInt("GO_MA_RELAY_LOW_WATER_MARK", defaultLowWaterMark)
+	highWaterMark       int           = env.GetInt("GO_MA_RELAY_HIGH_WATER_MARK", defaultHighWaterMark)
+	connmgrGracePeriod  time.Duration = env.GetDuration("GO_MA_RELAY_CONN_MGR_GRACE_PERIOD", defaultConnMgrGrace)
+	rendezvous          string        = env.Get("GO_MA_RELAY_RENDEZVOUS", "")
 )
 
 var (
@@ -49,8 +47,6 @@ func initConfig() {
 	flag.IntVar(&lowWaterMark, "lowWaterMark", lowWaterMark, "Low watermark for peer discovery")
 	flag.IntVar(&highWaterMark, "highWaterMark", highWaterMark, "High watermark for peer discovery")
 	flag.DurationVar(&connmgrGracePeriod, "connmgrGracePeriod", connmgrGracePeriod, "Grace period for connection manager")
-
-	flag.BoolVar(&enableRelayService, "enableRelayService", enableRelayService, "Enable circuit relay")
 
 	flag.StringVar(&httpAddr, "httpAddr", httpAddr, "Address to listen on")
 	flag.StringVar(&httpPort, "httpPort", httpPort, "Listen port for webserver")
